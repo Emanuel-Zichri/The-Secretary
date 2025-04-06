38e4cd5924fc59637b2292eba95808888d7d90e3
OshriBuskila <oshrib297@gmail.com>
2025-04-06 20:35:46 +0300

    הבנו

--- BL/Customer.cs
+++ BL/Customer.cs
@@ -12,7 +12,7 @@
         public string Street { get; set; }
         public string Number { get; set; }
         public string Notes { get; set; }
-        public string Password2 { get; set; }
+        public string Password3 { get; set; }
 
         public Customer() { }
 
